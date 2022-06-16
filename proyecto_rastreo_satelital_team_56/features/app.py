a04e7e14b062ed5940bc355c834523491f12f2c3
Alejandro Borda <alejandro.borda13@gmail.com>
2022-06-16 17:45:59 -0500

    Squashed commit of the following:
    commit 1c188818e2ca2c97a383909d32416aec08ed4e58
    Author: Alejandro Borda <alejandro.borda13@gmail.com>
    Date:   Sun Jun 12 20:35:38 2022 -0500
    
        add background to title, plots and styled map
    
    commit 0bcfbc27746a874f654c5585a3cf001500ddf2c3
    Author: dmateons <dmateons@gmail.com>
    Date:   Sat Jun 11 23:05:46 2022 -0500
    
        jugando con stilos

--- proyecto_rastreo_satelital_team_56/features/app.py
+++ proyecto_rastreo_satelital_team_56/features/app.py
@@ -2,6 +2,12 @@ import pandas as pd
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 from plotly import express as px
+from dotenv import load_dotenv
+import os
+
+map_token = os.getenv("MAPBOX_API_TOKEN")
+map_style = os.getenv("MAP_STYLE")
+load_dotenv("../../.env")
 
 #Import Data
 us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
@@ -10,18 +16,53 @@ hours = pd.DataFrame({"Hours":[x for x in range(24)], "Value":[x for x in range(
 
 #set the mapplot
 fig = px.line_mapbox(us_cities, lat="lat", lon="lon", zoom=12)
+<<<<<<< HEAD
 fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=10, mapbox_center_lat= 4.65,
     mapbox_center_lon= -74.1, margin={"r":0,"t":0,"l":0,"b":0})
+=======
+fig.update_layout(mapbox_zoom=10, mapbox_center_lat= 4.65,
+    mapbox_center_lon= -74.1, margin={"r":0,"t":0,"l":0,"b":0},
+    mapbox= dict(
+        accesstoken = map_token,
+        style = map_style
+    )
+)
+>>>>>>> fronted/styles
 
 #set the daysplot
-fig2 = px.bar(days, x="Day", y="Value", color="Value")
+data=[[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20]]
+fig2 = px.imshow(data,
+                labels=dict(x="Day of Week", y="Time of Day", color="Productivity"),
+                x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                y=['Morning', 'Afternoon', 'Evening']
+               )
+fig2.update_layout({
+    "plot_bgcolor": "rgba(0, 0, 0, 0)",
+    "paper_bgcolor": "rgba(0, 0, 0, 0)",
+    "font_color": "lightgrey",
+})
 
 #set the hoursplot
 fig3 = px.bar(hours, x="Hours", y="Value", color="Value")
 
+fig3.update_layout({
+    "plot_bgcolor": "rgba(0, 0, 0, 0)",
+    "paper_bgcolor": "rgba(0, 0, 0, 0)",
+    "font_color": "lightgrey",
+})
+
 # Create the app
-app = Dash(__name__, assets_url_path='./assets', external_stylesheets=[dbc.themes.BOOTSTRAP])
+url_theme1 = dbc.themes.FLATLY
+url_theme2 = dbc.themes.DARKLY
+dbc_css = (
+    "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+)
+app = Dash(__name__, assets_url_path='./assets', external_stylesheets=[url_theme1, dbc_css])
+
 app.title = 'Tiempos de viaje'
+
+main_blue= "#162f41"
+secondary_blue= "#2c3e50"
 
 # Layout
 def address__input(text):
@@ -37,6 +78,7 @@ def address__input(text):
     ])
 
 sidebar = dbc.Card([
+<<<<<<< HEAD
     address__input("origen"),
     address__input("destino"),
     dbc.Row(
@@ -54,6 +96,67 @@ sidebar = dbc.Card([
 titulo = [html.H1('Tiempos de viaje'),
     html.Hr(),
     html.P('Este dashboard muestra el tiempo de viaje de una ciudad a otra.')]
+=======
+    html.Div(
+        [
+            address__input("origen"),
+        ]),
+    html.Div(
+        [
+            address__input("destino"),
+        ]),
+    
+    html.Div(
+        [
+        html.Br(),
+        html.P('Range Slider', style={
+            'textAlign': 'center'
+        }),
+        dcc.RangeSlider(
+            id='range_slider',
+            min=0,
+            max=20,
+            step=0.5,
+            value=[5, 15]
+        ),
+        ]
+    ),
+    html.Div(
+        [
+            dbc.Card(
+                [dbc.Checklist(
+                    id='check_list',
+                    options=[{
+                        'label': 'Value One',
+                        'value': 'value1'
+                    },
+                        {
+                            'label': 'Value Two',
+                            'value': 'value2'
+                        },
+                        {
+                            'label': 'Value Three',
+                            'value': 'value3'
+                        }
+                    ],
+                    )
+                 ]),
+        ]
+    ),
+    html.Div(
+        [
+            dbc.Button("Estimar", outline=True, color="primary", className="me-1")
+        ]),
+    
+],body=True, style={"font_color":"white"}
+)
+
+titulo = [
+    html.H3('Tiempos de viaje', className="bg-primary text-white p-4 mb-2"),
+    html.Hr(style={"color": "#FFFFFF"}),
+    html.H6('Este dashboard muestra el tiempo de viaje de una ciudad a otra.', className="p-4 mb-2 text-white", style={"background-color": secondary_blue}),
+    ]
+>>>>>>> fronted/styles
 
 mapa =html.Div([
     dbc.Row([
@@ -65,17 +168,18 @@ mapa =html.Div([
 
 plots_bottom = dbc.Row([
     dbc.Col([
-        html.Div([
-            dcc.Graph(id='days__plot', figure=fig2)
-        ])
-    ]),
+        html.Div(
+            dcc.Graph(id='days__plot', figure=fig2, className="m-4")
+        )
+    ],md=5),
     dbc.Col([
         html.Div([
-            dcc.Graph(id='hours__plot', figure=fig3)
+            dcc.Graph(id='hours__plot', figure=fig3, className="m-4")
         ])
-    ]),
+    ],md=5),
 ])
 
+<<<<<<< HEAD
 app.layout = dbc.Container([
     *titulo,
     dbc.Row([
@@ -86,6 +190,27 @@ app.layout = dbc.Container([
             width={'size': 8, 'order': 2}),
     ]),
 ], fluid=True, style={"background-color": "#f5f5f5"})
+=======
+app.layout = dbc.Container(
+        [    
+            html.Div(titulo, className="p-4 mb-2", style={"margin": "10px"}),
+            dbc.Row(
+                [
+                    dbc.Col(sidebar, md=2, style={'background-color': main_blue}),
+                    dbc.Col(
+                        [                                
+                            dbc.Row(mapa),   
+                            dbc.Row(plots_bottom)
+                        ], md=10, style={'background-color': main_blue}
+                    )
+                ],
+            )
+        ],
+    className="dbc",
+    fluid=True,
+    style={"padding": "0px", "background-color": main_blue}
+)
+>>>>>>> fronted/styles
 
 # Callbacks
 # Start the server
